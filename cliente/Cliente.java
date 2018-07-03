89df6dbdd4c2be18e8ae04c61304272826076c1d
LucasSACastro <lucassebastiao2010@live.com>
2018-07-03 11:17:23 -0300

    Create Cliente.java
    Esqueleto do Cliente

---
+++ cliente/Cliente.java
@@ -0,0 +1,12 @@
+package cliente;
+
+class Cliente {
+	String cpf, nome, endereco, email;
+
+	Cliente(String cpf, String nome, String endereco, String email) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.email = email;
+	}
+}
